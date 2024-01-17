403b88ef4458cbf22a5681ef11a0c9f8a753b351
Samridh Gupta <samridhgupta1053@gmail.com>
2024-01-17 10:48:03 +0530

    third value

--- fc.py
+++ fc.py
@@ -1,10 +1,16 @@
 import random
 num_flips = 1000
-singlet_dict = {'Heads': 0, 'Tails': 0}
+doublet_dict = {'HH': 0, 'HT': 0, 'TH': 0, 'TT': 0}
 for _ in range(num_flips):
-    result = random.choice(['Heads', 'Tails'])
-    singlet_dict[result] += 1
-percentage_heads = (singlet_dict['Heads'] / num_flips) * 100
-percentage_tails = (singlet_dict['Tails'] / num_flips) * 100
-print(f'Heads: {percentage_heads:.2f}%')
-print(f'Tails: {percentage_tails:.2f}%')
+    result1 = random.choice(['H', 'T'])
+    result2 = random.choice(['H', 'T'])
+    doublet_combination = result1 + result2
+    doublet_dict[doublet_combination] += 1
+percentage_HH = (doublet_dict['HH'] / num_flips) * 100
+percentage_HT = (doublet_dict['HT'] / num_flips) * 100
+percentage_TH = (doublet_dict['TH'] / num_flips) * 100
+percentage_TT = (doublet_dict['TT'] / num_flips) * 100
+print(f'HH: {percentage_HH:.2f}%')
+print(f'HT: {percentage_HT:.2f}%')
+print(f'TH: {percentage_TH:.2f}%')
+print(f'TT: {percentage_TT:.2f}%')
