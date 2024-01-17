bc93affe0aa8668e208c0052e673a62ab0d2e3a5
Samridh Gupta <samridhgupta1053@gmail.com>
2024-01-17 10:42:57 +0530

    second value

--- fc.py
+++ fc.py
@@ -1,6 +1,10 @@
 import random
-flip=random.randint(0,1)
-if(flip==1):
-  print("It is Heads")
-elif(flip==0):
-  print("It is tail")
+num_flips = 1000
+singlet_dict = {'Heads': 0, 'Tails': 0}
+for _ in range(num_flips):
+    result = random.choice(['Heads', 'Tails'])
+    singlet_dict[result] += 1
+percentage_heads = (singlet_dict['Heads'] / num_flips) * 100
+percentage_tails = (singlet_dict['Tails'] / num_flips) * 100
+print(f'Heads: {percentage_heads:.2f}%')
+print(f'Tails: {percentage_tails:.2f}%')
