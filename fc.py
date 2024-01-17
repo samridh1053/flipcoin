4206ea96e798a99d8c605dcf447b27b72822201d
Samridh Gupta <samridhgupta1053@gmail.com>
2024-01-17 10:53:18 +0530

    fourth value

--- fc.py
+++ fc.py
@@ -1,16 +1,25 @@
 import random
 num_flips = 1000
-doublet_dict = {'HH': 0, 'HT': 0, 'TH': 0, 'TT': 0}
+triplet_dict = {'HHH': 0, 'HHT': 0, 'HTH': 0, 'THH': 0, 'HTT': 0, 'THT': 0, 'TTH': 0, 'TTT': 0}
 for _ in range(num_flips):
     result1 = random.choice(['H', 'T'])
     result2 = random.choice(['H', 'T'])
-    doublet_combination = result1 + result2
-    doublet_dict[doublet_combination] += 1
-percentage_HH = (doublet_dict['HH'] / num_flips) * 100
-percentage_HT = (doublet_dict['HT'] / num_flips) * 100
-percentage_TH = (doublet_dict['TH'] / num_flips) * 100
-percentage_TT = (doublet_dict['TT'] / num_flips) * 100
-print(f'HH: {percentage_HH:.2f}%')
-print(f'HT: {percentage_HT:.2f}%')
-print(f'TH: {percentage_TH:.2f}%')
-print(f'TT: {percentage_TT:.2f}%')
+    result3 = random.choice(['H', 'T'])
+    triplet_combination = result1 + result2 + result3
+    triplet_dict[triplet_combination] += 1
+percentage_HHH = (triplet_dict['HHH'] / num_flips) * 100
+percentage_HHT = (triplet_dict['HHT'] / num_flips) * 100
+percentage_HTH = (triplet_dict['HTH'] / num_flips) * 100
+percentage_THH = (triplet_dict['THH'] / num_flips) * 100
+percentage_HTT = (triplet_dict['HTT'] / num_flips) * 100
+percentage_THT = (triplet_dict['THT'] / num_flips) * 100
+percentage_TTH = (triplet_dict['TTH'] / num_flips) * 100
+percentage_TTT = (triplet_dict['TTT'] / num_flips) * 100
+print(f'HHH: {percentage_HHH:.2f}%')
+print(f'HHT: {percentage_HHT:.2f}%')
+print(f'HTH: {percentage_HTH:.2f}%')
+print(f'THH: {percentage_THH:.2f}%')
+print(f'HTT: {percentage_HTT:.2f}%')
+print(f'THT: {percentage_THT:.2f}%')
+print(f'TTH: {percentage_TTH:.2f}%')
+print(f'TTT: {percentage_TTT:.2f}%')
